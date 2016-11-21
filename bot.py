44216d714e0061b59d15b0dd6b20bdf3dd2c80ef
Taehwan Kim <maxtortime@gmail.com>
2016-11-21 21:53:07 +0900

    org 회원 알아내서 메시지 출력

--- bot.py
+++ bot.py
@@ -7,16 +7,17 @@ import os
 
 sched = BlockingScheduler()
 local_tz = pytz.timezone('Asia/Seoul')
-token = os.environ['SLACK_TOKEN']
+slack_token = os.environ['SLACK_TOKEN']
+gh_token = os.environ['GITHUB_ACCESS_TOKEN']
 
-slack = Slacker(token)
-channels = ['#_general', '#announcements','#test_bot']
+slack = Slacker(slack_token)
+channels = ['#_general', '#announcements', '#test_bot']
 
 def post_to_channel(message, idx):
     slack.chat.post_message(channels[idx], message, as_user=True)
 
-def get_repo_last_commit_delta_time(owner, repo):
-    repo = github3.repository(owner, repo)
+def get_repo_last_commit_delta_time(owner, repo_name):
+    repo = github3.repository(owner, repo_name)
     return repo.pushed_at.astimezone(local_tz)
 
 def get_delta_time(last_commit):
@@ -24,28 +25,29 @@ def get_delta_time(last_commit):
     delta = now - last_commit
     return delta.days
 
+
+# uncomment when testing
+# @sched.scheduled_job('interval', seconds=3)
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=14)
 def main():
-    members = (
-        # (git 계정 이름, repo 이름, 이름),
-        # [...]
-        ('KangTrue','TMLgorithm','강진실'),
-        ('maxtortime','TMLgorithm','김태환'),
-        ('QBlek','TMLgorithm','한남규'),
-        ('Rekdoll','TMLgorithm','현민성'),
-        ('rerong','TMLgorithm','이상란'),
-    )
+    gh = github3.login(token=gh_token)
+    tml = gh.organization(login='ajoutml')
     reports = []
 
-    for owner, repo, name in members:
-        last_commit = get_repo_last_commit_delta_time(owner, repo)
+    for member in tml.iter_members():
+        owner = member.login
+        last_commit = get_repo_last_commit_delta_time(owner, 'TMLgorithm')
         delta_time = get_delta_time(last_commit)
 
         if(delta_time == 0):
-            reports.append('*%s* 님은 오늘 커밋을 하셨어요!' % (name))
+            reports.append('*%s* 님은 오늘 커밋을 하셨어요!'
+                           % (owner))
         else:
-            reports.append('*%s* 님은 *%s* 일이나 커밋을 안하셨어요!' % (name, delta_time))
+            reports.append('*%s* 님은 *%s* 일이나 커밋을 안하셨어요!'
+                           % (owner, delta_time))
 
-    post_to_channel('\n 안녕 친구들! 과제 점검하는 커밋벨이야 호호 \n' + '\n'.join(reports), 0)
+    post_to_channel('\n 안녕 친구들! 과제 점검하는 커밋벨이야 호호 \n'
+                    + '\n'.join(reports), 0)
 
-sched.start()
+if __name__ == '__main__':
+    sched.start()
